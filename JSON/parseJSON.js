a6a6dcf765bce39676f7f0a7cbad3e01a47a9bdc
Linus <linus.yan87@gmail.com>
2017-03-03 15:52:58 +0800

    Create JSON/parseJSON.js

---
+++ JSON/parseJSON.js
@@ -0,0 +1,23 @@
+var rvalidchars = /^[\],:{}\s]*$/;
+var rvalidescape = /\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g;
+var rvalidtokens = /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g;
+var rvalidbraces = /(?:^|:|,)(?:\s*\[)+/g;
+var rtrimLeft = /^\s+/;
+var rtrimRight = /\s+$/;
+	
+exports = function parsejson(data) {
+	if ('string' != typeof data || !data) {
+		return null;
+	}
+
+	data = data.replace(rtrimLeft, '').replace(rtrimRight, '');
+
+	// Attempt to parse using the native JSON parser first
+	if (global.JSON && JSON.parse) {
+		return JSON.parse(data);
+	}
+
+	if (rvalidchars.test(data.replace(rvalidescape, '@').replace(rvalidtokens, ']').replace(rvalidbraces, ''))) {
+		return (new Function('return ' + data))();
+	}
+};
